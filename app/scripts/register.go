027c9f964320dd340e0446ccfc5b6a36937a9a38
dengyongcai <qq@example.com>
2024-06-05 23:45:31 +0800

    opt:完善k线逻辑

--- app/scripts/register.go
+++ app/scripts/register.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/luxun9527/gex/app/account/rpc/pb"
+	"github.com/zeromicro/go-zero/core/stringx"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:20002", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Panicf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+	cli := pb.NewAccountServiceClient(conn)
+	ctx := context.Background()
+	for i := 0; i < 100; i++ {
+		username := fmt.Sprintf("test%d", i+1)
+		password := stringx.Randn(8)
+		result, err := cli.Register(ctx, &pb.RegisterReq{
+			Username:    username,
+			Password:    password,
+			PhoneNumber: time.Now().Unix(),
+		})
+		if err != nil {
+			log.Panicf("Failed to register: %v", err)
+		}
+
+		_, err = cli.AddUserAsset(ctx, &pb.AddUserAssetReq{
+			Uid:      result.Uid,
+			CoinName: "USDT",
+			Qty:      "10000000",
+		})
+		if err != nil {
+			log.Panicf("Failed to add user asset: %v", err)
+		}
+		_, err = cli.AddUserAsset(ctx, &pb.AddUserAssetReq{
+			Uid:      result.Uid,
+			CoinName: "IKUN",
+			Qty:      "1000",
+		})
+		if err != nil {
+			log.Panicf("Failed to add user asset: %v", err)
+		}
+		fmt.Printf("uid %v username %v password %v\n", result.Uid, username, password)
+
+	}
+}
